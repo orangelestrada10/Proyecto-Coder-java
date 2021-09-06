1a2783f956ddff8aac7de0f950ed6b6b7ccd5539
orangel <orangelestradasanz@gmail.com>
2021-09-06 20:45:46 -0300

    ejercicio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-let nombre = prompt("Ingrese su nombre");
-alert(nombre);
-let apellido = prompt("Ingrese Su Apellido");
-alert(apellido);
+//let nombre = prompt("Ingrese su nombre");
+//alert(nombre);    
+//let apellido = prompt("Ingrese Su Apellido");
+//alert(apellido);
+//let edadUsuario = prompt("Ingrese su edad");
+//let edad = parseInt(edadUsuario);
+//let numero = 20;
+//console.log(edad+numero);
+
+let usuario = "usuario"
+let contraseña = "contraseña"
+let usuarioIngresado = prompt("Ingrese Nombre Usuario");
+let contraseñaIngresada = prompt("Ingrese Contraseña");
+
+if (usuarioIngresado == usuario && contraseñaIngresada == contraseña) {
+    alert ("Bienvenido");
+}
+else{
+    alert("No se pudo ingresar");
+}
+
+let menuDia = prompt("Ingrese su menu");
+if (menuDia = "Hamburguesa"){
+    alert("Si es el menu de el dia")
+}
+else if (menuDia = "pollo"){
+    alert("No es menu de el dia");
+}
+else {
+    alert("No es menu de el dia");
+}
+
 let edadUsuario = prompt("Ingrese su edad");
 let edad = parseInt(edadUsuario);
-let numero = 20;
-console.log(edad+numero);
+if(20 == edad ){
+    alert("puedes entrar")
+}
+
+else{
+    alert("No puedes Entrar")
+}
