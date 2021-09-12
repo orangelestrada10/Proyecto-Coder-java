28b3f056fec0c9642b014a48bf5a6f6a21856d3d
orangel <orangelestradasanz@gmail.com>
2021-09-12 12:18:55 -0300

    arreglo

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-let inicio = prompt("Ingrese Tipo de vacuna contra COVID-19");
+const suma = (a,b) => a+b;
 
-while(inicio != "no" ){
-   switch (inicio) {
-       case "sputnik":
-            alert("Tu Vacuna es la sputnik");
-            break;
-        case "sinopharm":
-            alert("Tu Vacuna es la sinopharm");
-            break;
-        case "pfizer":
-            alert("Tu Vacuna es la pfizer");
-            break;
-       default:
-           alert("No Hay Vacuna")
-           break;
-   }
-   inicio = prompt("Ingrese Tipo de vacuna contra COVID-19");
-}
-for(let turno = 1; turno <=3; turno++){
-    let paciente = prompt("Ingresar Nombre del paciente " );
-    while(paciente == ""){
-        paciente = prompt("No se ingreso paciente, ingrese uno")
-    }
-    let ingresarVacuna = prompt("Ingresar Vacuna");
-    while(ingresarVacuna == ""){
-        ingresarVacuna = prompt("No se ingreso una vacuna, Porfavor Ingresar una nuevamente!");
-    }
-    console.log(" Nombre del paciente: "+  paciente + " Tipo de Vacuna: " + ingresarVacuna);
+const resta = (a,b) => a-b;
+
+const iva = (x) => x*0.21;
+
+const descuento = (x) => x*0.15;
+
+const cuotas = (x,nCuotas) => x / nCuotas;
+
+
+function procesarProducto(nombre, valor, numeroCuotas){
+    console.log(nombre + " con iva: " + suma(valor,iva(valor)) + "$");
+    console.log(nombre + " con descuento del 15%: " + resta(valor,descuento(valor)) + "$" );
+    if(numeroCuotas == 1)
+        console.log(nombre + " a " + numeroCuotas + " cuota: " + cuotas(valor,numeroCuotas));
+    else
+        console.log(nombre + " a " + numeroCuotas + " cuotas: " + cuotas(valor,numeroCuotas));
 }
 
+let sumaNeta = 0;
 
-    
+for (let i = 1; i <= 3; i++) {
+    let producto = prompt("Ingrese el nombre del producto " + i );
+    let valorProducto = parseInt(prompt("Ingrese el valor del producto " + i ));
+    let mesesCuotas = parseInt(prompt("Ingrese la cantidad de cuotas deseadas"));
+    procesarProducto(producto,valorProducto,mesesCuotas);
+
+    sumaNeta = sumaNeta + valorProducto; 
+}
+
+console.log("Suma total: "+ sumaNeta)
+console.log("Suma total con iva: "+ suma(sumaNeta, iva(sumaNeta)))
+console.log("Suma total con descuento del 15%: "+ resta(sumaNeta, descuento(sumaNeta)))
