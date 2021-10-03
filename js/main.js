005d55f23273294c49b2e1557ce6d8a6a2a5decc
orangel <orangelestradasanz@gmail.com>
2021-10-03 15:31:13 -0300

    listo

--- js/main.js
+++ js/main.js
@@ -13,116 +13,144 @@ class Usuario {
 }
 
 class Cancha {
-  constructor(nombre) {
-      this.nombre = nombre
-      this.reserva = ""
-  }
+    constructor(nombre, reserva = "") {
+        this.nombre = nombre
+        this.reserva = reserva
+    }
 
-  reservar(username){
-      if(this.reserva == "")
-          this.reserva = username
-      else
-          alert("La cancha "+ this.nombre + " ya esta reservada")
-  }
+    reservar(username){
+        if(this.reserva == "")
+            this.reserva = username
+        else
+            alert("La cancha "+ this.nombre + " ya esta reservada")
+    }
 }
 
 let usuarios = []
 
 let canchas = [
-  new Cancha("La Bombonera"),
-  new Cancha("Monumental"),
-  new Cancha("Estadio Libertadores de America"),
-  new Cancha("Camp Nou"),
-  new Cancha("Anfield"),
+    new Cancha("La Bombonera"),
+    new Cancha("Monumental"),
+    new Cancha("Estadio Libertadores de America"),
+    new Cancha("Camp Nou"),
+    new Cancha("Anfield"),
 ]
 
 let usuarioLogueado
 
 function login() {
-  const username = prompt("Introduzca Nombre de Usuario")
-  const pass = prompt("Introduzca Contraseña")
+    const username = prompt("Introduzca Nombre de Usuario")
+    const pass = prompt("Introduzca Contraseña")
 
-  let usuario = usuarios.find( user => user.username == username && user.pass == pass )
+    let usuario = usuarios.find( user => user.username == username && user.pass == pass )
 
-  if(usuario)
-      usuarioLogueado = usuario
-  else
-      alert("Usuario o contraseña incorrectos")
+    if(usuario)
+        usuarioLogueado = usuario
+    else
+        alert("Usuario o contraseña incorrectos")
 }
 
 function registrarse() {
-  let username = "", pass = "", nombre = "", apellido = "", equipo = "";
+    let username = "", pass = "", nombre = "", apellido = "", equipo = "";
 
-  do {
-      username = prompt("Ingrese Nombre de Usuario")
-      if(username == "")
-          alert("El username no puede ser vacio")
-  }while(username == "")
+    let EsUsernameInvalido;
 
-  do {
-      pass = prompt("Ingrese Contraseña")
-      if(pass == "")
-          alert("El password no puede ser vacio")
-  }while(pass == "")
+    do {
+        EsUsernameInvalido = false;
+        username = prompt("Ingrese Nombre de Usuario")
+        if(username == ""){
+            alert("El username no puede ser vacio")
+            EsUsernameInvalido = true
+        }
+        else if(usuarios.find(user => user.username == username)){
+            alert("Ese username ya esta en uso")
+            EsUsernameInvalido = true
+        }   
+    }while(EsUsernameInvalido)
 
-  do {
-      nombre = prompt("Ingrese Nombre")
-      if(nombre == "")
-          alert("El Nombre no puede estar vacio")
-  }while(nombre == "")
+    do {
+        pass = prompt("Ingrese Contraseña")
+        if(pass == "")
+            alert("El password no puede ser vacio")
+    }while(pass == "")
 
-  do {
-      apellido = prompt("Ingrese Apellido")
-      if(apellido == "")
-          alert("El apellido no puede ser vacio")
-  }while(apellido == "")
+    do {
+        nombre = prompt("Ingrese Nombre")
+        if(nombre == "")
+            alert("El Nombre no puede estar vacio")
+    }while(nombre == "")
 
-  do {
-      equipo = prompt("Ingrese Nombre de el equipo")
-      if(equipo == "")
-          alert("El nombre del equipo no puede ser vacio")
-  }while(equipo == "")
+    do {
+        apellido = prompt("Ingrese Apellido")
+        if(apellido == "")
+            alert("El apellido no puede ser vacio")
+    }while(apellido == "")
 
-  usuarios.push(new Usuario(username, pass , nombre, apellido, equipo))
+    do {
+        equipo = prompt("Ingrese Nombre de el equipo")
+        if(equipo == "")
+            alert("El nombre del equipo no puede ser vacio")
+    }while(equipo == "")
+
+    usuarios.push(new Usuario(username, pass , nombre, apellido, equipo))
   
 }
 
 function menuUsuario() {
-  let operacion = 0
-  while(operacion != 3) {
-      operacion = parseInt(prompt("------- Bienvenido "+ usuarioLogueado.nombre + " --------\n1. Reservar\n2. Mis Reservas\n3. Cerrar sesion"))
 
-      let textoMenu
+    document.body.innerHTML ="<h1>Bienvenido "+ usuarioLogueado.nombre + "</h1>"
 
-      switch(operacion) {
-          case 1:
-              textoMenu = "- Que cancha quieres reservar? -\n"
-              for (let i = 0; i < canchas.length; i++) {
-                  const cancha = canchas[i];
+    for (const cancha of canchas) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h3> Nombre: ${cancha.nombre}</h3>`
 
-                  textoMenu = textoMenu + (i+1) + ". " + cancha.nombre + "\n"
-              }
-              let nCancha = prompt(textoMenu)
+        if(cancha.reserva == "") {
+            let botonReserva = document.createElement("button");
+            botonReserva.innerHTML = "Reservar"
+        
+            botonReserva.addEventListener("click", function() {
+                cancha.reservar(usuarioLogueado.username)
+                guardarDatos()
+                menuUsuario()
+            });
+            contenedor.appendChild(botonReserva);
+        }
+        else 
+            contenedor.innerHTML += `<p>Esta Reservado por ${cancha.reserva}</p>`
+   
+        document.body.appendChild(contenedor);
+    }
 
-              if(nCancha > 0 && nCancha <= canchas.length)
-                  canchas[nCancha-1].reservar(usuarioLogueado.username)
-              else
-                  alert("Numero de cancha invalido")
-              break;
-          case 2:
-              textoMenu = "------- Mis Reservas -------\n"
-              for (const cancha of canchas.sort(comparaCanchas)) {
-                  if(cancha.reserva == usuarioLogueado.username)
-                      textoMenu = textoMenu + cancha.nombre + "\n" 
-              }
-              alert(textoMenu)
-              break;
-          case 3:
-              alert("Cerrando Sesion.")
-              break;
-      }
-  }
-  usuarioLogueado = null
+    let botonMisReservas = document.createElement("button");
+    botonMisReservas.innerHTML = "Ver Mis Reservas"
+
+    botonMisReservas.addEventListener("click", function() {
+        misReservas()
+    });
+                            
+    document.body.appendChild(botonMisReservas);
+}
+
+function misReservas (){
+    document.body.innerHTML ="<h1>Mis Reservas</h1>"
+
+    for (const cancha of canchas.sort(comparaCanchas)) {
+        if(cancha.reserva == usuarioLogueado.username) {
+            let contenedor = document.createElement("div");
+            contenedor.innerHTML = `<h3> Nombre: ${cancha.nombre}</h3>`
+                                    
+            document.body.appendChild(contenedor);
+        }
+    }
+
+    let botonVolver = document.createElement("button");
+    botonVolver.innerHTML = "Volver"
+
+    botonVolver.addEventListener("click", function() {
+        menuUsuario()
+    });
+                            
+    document.body.appendChild(botonVolver);
 }
 
 function comparaCanchas( a, b ) {
@@ -135,62 +163,54 @@ function comparaCanchas( a, b ) {
   return 0;
 }
 
+function leerDatos(){
+    let datos = localStorage.getItem("datos");
+
+    if(!datos) {
+        guardarDatos()
+    }
+    else {
+        let datosParseados = JSON.parse(datos); 
+        usuarios = []
+        canchas = []
+
+        for(usuario of datosParseados.usuarios) {
+            usuarios.push(new Usuario(usuario.username, usuario.pass, usuario.nombre, usuario.apellido, usuario.equipo ))
+        }
+        for(cancha of datosParseados.canchas) {
+            canchas.push(new Cancha(cancha.nombre, cancha.reserva))
+        }
+    }
+}
+
+function guardarDatos() {
+    let datos = `{"usuarios": ${JSON.stringify(usuarios)}, "canchas": ${JSON.stringify(canchas)} }`
+    localStorage.setItem('datos', datos);
+
+    console.log("Se han guardado datos en local Storage")
+}
+
+ 
+
+leerDatos()
 let operacion = 0
 while(operacion != 3) {
-  // Imprime el menu principal y espera que escribas en el prompt un numero
   operacion = parseInt(prompt("------- Menu --------\n1. Iniciar sesion\n2. Registrarse\n3. Salir"))
 
   switch(operacion) {
       case 1:
           login()
-          if(usuarioLogueado)
-              menuUsuario()
+          if(usuarioLogueado){
+            menuUsuario()
+            operacion = 3
+          }
           break;
       case 2:
           registrarse()
+          guardarDatos()
           break;
       case 3:
           alert("Hasta luego!")
           break;
   }
 }
-const productos = [{ jugadores: 1,  nombre: "Messi", equipo: "PSG" },
-                  {  jugadores: 2,  nombre: "Di maria", equipo: "PSG"},
-                  {  jugadores: 3,  nombre: "Paredes"  , equipo: "PSG"},
-                  {  jugadores: 4,  nombre: "Dybala" , equipo: "JUVENTUS"}]
-for (const producto of productos) {
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<h3> Nro: ${producto.jugadores}</h3>
-                            <p>  Nombres: ${producto.nombre}</p>
-                            <b> Equipo: ${producto.equipo}</b>`;
-    document.body.appendChild(contenedor);
-}
-
-
-class Nombre {
-    constructor(nombre){
-        this.nombre = nombre;
-    }
-}
-
-class Equipo {
-    constructor(nombre) {
-        this.nombre = nombre;
-    }
-
-    mostrarEquipo() {
-        const section = document.createElement("section");
-        const nombre = document.createElement("h1");
-        nombre.textContent = `Bienvenido a ${this.nombre}`;
-        section.appendChild(nombre);
-        document.body.appendChild(section);
-    }
-  }
-
-
-function iniciarPrograma() {
-    const equipo = new Equipo(prompt("Nombre Equipo"));
-    equipo.mostrarEquipo();
-}
-
-iniciarPrograma();
